0982cc26e893a9bb4433608dd45dacc3664f0d91
SonnyJim <newbey009@gmail.com>
2024-04-10 14:06:33 -0500

    staged

---
+++ demo/src/main/java/miu/cs425/demo/Lab6Application.java
@@ -0,0 +1,13 @@
+package miu.cs425.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab6Application.class, args);
+	}
+
+}
